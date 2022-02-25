239673037118f4012c3fcd6430ad306b2f2ad434
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2022-02-25 17:21:51 -0600

    Create WelcomeScreen.js
    Verifies and Navigates using formik, yup and navigation

---
+++ WelcomeScreen.js
@@ -0,0 +1,43 @@
+//WelcomeScreen.js
+//template from https://blog.logrocket.com/react-native-form-validations-with-formik-and-yup/ and Youtube Channel "Pradip Debnath"
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Button
+} from 'react-native'
+import SignUp from './SignUpScreen'
+
+const WelcomeScreen = ({navigation}) => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+          <Text>Welcome Screen</Text>
+          <Button
+            title="Sign out"
+            onPress={()=> navigation.navigate("SignIn")}
+        />
+      </SafeAreaView>
+    </>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+export default WelcomeScreen
