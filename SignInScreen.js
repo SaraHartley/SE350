9469e19f8dbd1dba1f215d67c89b373376ee2ee4
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2022-02-25 17:18:47 -0600

    Create SignInScreen.js
    Verifies and Navigates using formik, yup and navigation

---
+++ SignInScreen.js
@@ -0,0 +1,124 @@
+//template from https://blog.logrocket.com/react-native-form-validations-with-formik-and-yup/ and Youtube Channel "Pradip Debnath"
+//SignInScreen.js
+import React from 'react'
+import { Formik } from 'formik'
+import * as yup from 'yup'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Button
+} from 'react-native'
+import SignUp from './SignUpScreen'
+
+const SignInScreen = ({navigation}) => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.loginContainer}>
+          <Text>Login Screen</Text>
+          <Formik
+            validationSchema={loginValidationSchema}
+            initialValues={{ email: '', password: '' }}
+            onSubmit={values => console.log(values)}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched, isValid, }) => (
+              <>
+                <TextInput
+                  name="email"
+                  placeholder="Email Address"
+                  style={styles.textInput}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                />
+                {(errors.email && touched.email) &&
+                  <Text style={styles.errorText}>{errors.email}</Text>
+                }
+                <TextInput
+                  name="password"
+                  placeholder="Password"
+                  style={styles.textInput}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  secureTextEntry
+                />
+                {(errors.password && touched.password) &&
+                  <Text style={styles.errorText}>{errors.password}</Text>
+                }
+                <Button 
+                  onPress={handleSubmit}
+                  title="LOGIN"
+                  disabled={!isValid} 
+                />
+                <Button
+                  title="Go to SignUp screen"
+                  onPress={()=> navigation.navigate("SignUp")}
+                />
+                <Button
+                  title="Go to welcome screen"
+                  onPress={()=> navigation.navigate("Welcome")}
+                />
+                <Button
+                  title="Google SignIn"
+                  onPress={()=> navigation.navigate("Welcome")}
+                />
+              </>
+            )}
+          </Formik>
+        </View>
+      </SafeAreaView>
+    </>
+  )
+}
+const loginValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter valid email")
+    .required('Email Address is Required'),
+  password: yup
+    .string()
+    .min(8, ({ min }) => `Password must be at least ${min} characters`)
+    .required('Password is required'),
+})
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginContainer: {
+    width: '80%',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    elevation: 10,
+    backgroundColor: '#e6e6e6'
+  },
+  textInput: {
+    height: 40,
+    width: '100%',
+    margin: 10,
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 10,
+  },
+  errorText: {
+    fontSize: 10,
+    color: 'red',
+  },
+})
+
+export default SignInScreen
