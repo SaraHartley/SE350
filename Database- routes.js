243d435ca4a90c36298678a66f06af679e31bef1
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2022-03-17 14:15:24 -0500

    Update Database- routes.js
    right code

--- Database- routes.js
+++ Database- routes.js
@@ -1,81 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View,Button,SafeAreaView,AppRegistry,TextInput } from 'react-native';
-import React,{Component} from 'react';
- //iadd rqact native server libraryu
-// import routes from './routes'
+//routes.js
+//this is our simple Nodejs server
 
-class App extends Component{
-  onPressButton=()=>{
-    alert('alert from function test')
-    console.log("user pressed button from function test")
 
-    fetch('http://192.168.254.79:3000/users')
-    //fetch('http://localhost:3000/users')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
-    .then(response => response.json())
-    //.then(users => console.warn(users))
-    .then(users => console.log(users))
-    //.then(users => alert(users))
-    //alert(users)
-    
-
-  };
-  
-  render(){
-    //const [text,onChangeText] = useState("");
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text>Open up App.js to start working on your 350-database-test app!</Text>
-        {/*<TextInput>
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-        </TextInput>*/}
-        <Button
-          //onPress={this.test2}
-          onPress={this.onPressButton}
-
-          title="Button"
-          //onPress={()=>alert('pressed')}
-        />
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    );
-  };
-};
-//works!
-//function test2(){
-//  alert('alert from test2')
-//};
-
-//doesn't work
-function test(){
-  alert('alert from function test')
-  console.log("user pressed button from function test")
-
-  //fetch('http://192.168.254.79:3000/users')
-  fetch('http://localhost:3000/users')
-
-    .then(response => response.json())
-    //.then(users => console.warn(users))
-    .then(users => console.log(users))
-    //.then(users => alert(users))
-
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input:{
-    margin:15,
-    borderColor: 'black',
-    borderWidth: 1
-  },
+const connection = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'ReactDb'
 });
 
-export default App;
+// Starting our app.
+const app = express();
+
+// Creating a GET route that returns data from the 'users' table.
+app.get('/users', function (req, res) {
+    // Connecting to the database.
+    connection.getConnection(function (err, connection) {
+
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query('SELECT * FROM users', function (error, results, fields) {
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
+
+// Starting our server.
+app.listen(3000, () => {
+ console.log('Go to http://localhost:3000/users so you can see the data.');
+});
+
