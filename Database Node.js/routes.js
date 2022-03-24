49db7b770f67a91db3e0263882d5c742378580b1
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2022-03-23 20:15:27 -0500

    Update routes.js
    doesn't use user input

--- Database Node.js/routes.js
+++ Database Node.js/routes.js
@@ -1,12 +1,14 @@
 //routes.js
 //this is our simple Nodejs server
 
+//require('dotenv').config(); //instatiate environment variables
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
 const connection = mysql.createPool({
+  //connectionLimit : CONFIG.db_connectionlimit,
   host     : 'localhost',
   user     : 'root',
   password : '',
@@ -15,6 +17,31 @@ const connection = mysql.createPool({
 
 // Starting our app.
 const app = express();
+
+app.post('/newUser', function (req, res) {
+  //console.log(req.body.username);           //!!!! This is the line that causes the error!!!
+  console.log("at post");
+
+  // Connecting to the database.
+  connection.getConnection(function (err, connection) {
+
+  // Checking for errors.
+  if(err) throw err;
+  console.log("Connected!");
+
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query("INSERT INTO users (username) values('Mike')", function (error, results, fields) {
+    //connection.query("INSERT INTO users (username) values(req.body.username)", function (error, results, fields) {
+
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(JSON.stringify(results));
+    });
+  });
+});
+
 
 // Creating a GET route that returns data from the 'users' table.
 app.get('/users', function (req, res) {
@@ -36,4 +63,3 @@ app.get('/users', function (req, res) {
 app.listen(3000, () => {
  console.log('Go to http://localhost:3000/users so you can see the data.');
 });
-
