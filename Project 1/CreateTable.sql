cb3e870a95310ace94ebb51afb7b9ffa01bc11f3
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2022-03-28 15:31:49 -0500

    Create CreateTable.sql

---
+++ Project 1/CreateTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `rorrusers` (
+  `rorrId` int(11) NOT NULL AUTO_INCREMENT,
+  `rorrEmail` varchar(40) NOT NULL,
+  `rorrPassword` varchar(40) NOT NULL,
+  `rorrFullName` varchar(40) NOT NULL,
+  PRIMARY KEY (`rorrId`),
+  UNIQUE KEY `rorrEmail` (`rorrEmail`),
+  UNIQUE KEY `rorrPassword` (`rorrPassword`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4
