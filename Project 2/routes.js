5c7aaaf67dc9f7d75464964e05cbd5c9c90f8c00
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2022-04-07 17:21:48 -0500

    Create routes.js

---
+++ Project 2/routes.js
@@ -0,0 +1,78 @@
+//routes.js
+//this is our simple Nodejs server
+
+//require('dotenv').config(); //instatiate environment variables
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const connection = mysql.createPool({
+  //connectionLimit : CONFIG.db_connectionlimit,
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'ReactDb'
+});
+
+// Starting our app.
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.post('/newCount', function (req, res) {
+  console.log(req.body.Animal);
+  console.log(req.body.Funny);
+  console.log(req.body.News);
+  console.log(req.body.Political);
+
+
+  console.log("at post");
+
+  // Connecting to the database.
+  connection.getConnection(function (err, connection) {
+
+  // Checking for errors.
+  if(err) throw err;
+  console.log("Connected!");
+
+    // Executing the MySQL query (select all data from the 'users' table).
+    //connection.query("INSERT INTO users (username) values('Mike')", function (error, results, fields) {
+
+    var animal = req.body.Animal;
+    var funny =req.body.Funny;
+    var news =req.body.News;
+    var political =req.body.Political;
+
+    connection.query(`INSERT INTO topicCounter (Animal,Funny,News,Political) values("${animal}","${funny}","${news}","${political}")`, function (error, results, fields) {
+
+
+      if (error) throw error;
+      
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(JSON.stringify(results));
+    });
+  });
+});
+
+
+// Creating a GET route that returns data from the 'users' table.
+app.get('/getCount', function (req, res) {
+    // Connecting to the database.
+    connection.getConnection(function (err, connection) {
+
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query('SELECT Animal,Funny,News,Political FROM topicCounter', function (error, results, fields) {
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
+
+// Starting our server.
+app.listen(3000, () => {
+ console.log('Go to http://localhost:3000/getCount so you can see the data.');
+});
