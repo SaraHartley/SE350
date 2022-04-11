2b718f18f662820333248dff6b0ceed8a319553d
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2022-04-11 14:50:37 -0500

    Update App.js

--- Project 2/App.js
+++ Project 2/App.js
@@ -5,11 +5,41 @@
 
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput,SafeAreaView,FlatList,TouchableOpacity,Image } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput,SafeAreaView,FlatList,TouchableOpacity,Image,AppState } from 'react-native';
+import React,{useRef,useState,useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
 export default function App() {
+
+  //auto session stuff
+  const appState = useRef(AppState.currentState)
+  const [appStateVisible, setAppStateVisible] = useState(appState.current)
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        console.log("App has come to the foreground!");
+      }
+
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+      console.log("AppState", appState.current);
+       if (appState.current.match(/inactive|background/))
+       {
+         console.log("send data here");
+         getTest();
+       }
+    });
+    return () => {
+    subscription.remove();
+    };
+    }, []);
+  
+
   //grid view stuff
   const animalPic='http://boomstickcomics.com/wp-content/uploads/2015/07/the_revenant__2015__movie_poster_by_nabilstevieg-d8jq7yv.jpg';
   const funnyPic='http://boomstickcomics.com/wp-content/uploads/2015/07/the_revenant__2015__movie_poster_by_nabilstevieg-d8jq7yv.jpg';
@@ -40,11 +70,11 @@ export default function App() {
   ]
   
   //Database stuff
-  async function postTest(animalCount,funnyCount,newsCount,politicalCount){
-    animalCount+=1;
-    funnyCount+=1;
-    newsCount+=1;
-    politicalCount+=1;
+  async function postTest(inputAnimalCount,inputFunnyCount,inputNewsCount,inputPoliticalCount){
+    inputAnimalCount+=animalCounter;
+    inputFunnyCount+=funnyCounter;
+    inputNewsCount+=newsCounter;
+    inputPoliticalCount+=politicalCounter;
     console.log("Inside postTest");
     await fetch('http://192.168.254.79:3000/newCount', {
       method: 'POST', // Here you're saying that you want to make a POST request. Could be any method, like a GET, for example.
@@ -55,10 +85,10 @@ export default function App() {
       body: JSON.stringify({ // Here's the fun part. Put your data here.
         //"username": this.state.name,
         //username: username,
-        Animal: animalCount,
-        Funny: funnyCount,
-        News: newsCount,
-        Political: politicalCount
+        Animal: inputAnimalCount,
+        Funny: inputFunnyCount,
+        News: inputNewsCount,
+        Political: inputPoliticalCount
 
       })
       
@@ -71,6 +101,7 @@ export default function App() {
   function getTest(){
     //search if either username already in DB
     console.log("inside getTEST");
+    //alert('inside gettest');
     var value = true;
     var tempResponse = {};
     fetch('http://192.168.254.79:3000/getCount')
@@ -79,17 +110,23 @@ export default function App() {
       .then(users => {
         var count = 0;
         console.log(`length ${users.length}`);
+        //alert(`length ${users.length}`);
         var lastElement= users[users.length-1];
         console.log(lastElement);
+        //alert(lastElement);
         var animalCount=lastElement.Animal;
         var funnyCount=lastElement.Funny;
         var newsCount=lastElement.News;
         var politicalCount=lastElement.Political;
 
         console.log(animalCount);
+        //alert(animalCount);
         console.log(funnyCount);
+        //alert(funnyCount);
         console.log(newsCount);
+        //alert(newsCount);
         console.log(politicalCount);
+        //alert(politicalCount);
 
         postTest(animalCount,funnyCount,newsCount,politicalCount);
       })
@@ -110,21 +147,29 @@ export default function App() {
 
   const getData = async () => {
     //Multiget
-    //const values = await AsyncStorage.multiGet(['@count', '@greeting']);
+    const values = await AsyncStorage.multiGet(['@count', '@animalCount','@funnyCount','@newsCount','@politicalCount']);
 
-    /*values.forEach(value => {
+    values.forEach(value => {
       if (value[0] === '@count') {
         const count = parseInt(value[1]);
         setCounter(isNaN(count) ? 0 : count);
-      } else if (value[0] === '@greeting') {
-        setGreetingInfo(JSON.parse(value[1]));
+      } else if (value[0] === '@animalCount') {
+        setAnimalCounter(JSON.parse(value[1]));
+      }else if (value[0] === '@funnyCount') {
+        setFunnyCounter(JSON.parse(value[1]));
+      }else if (value[0] === '@newsCount') {
+        setNewsCounter(JSON.parse(value[1]));
+      }else if (value[0] === '@politicalCount') {
+        setPoliticalCounter(JSON.parse(value[1]));
       }
-    });*/
+    });
 
     // Alternative is to call getItem for each key but better to use multiGet if getting multiple keys
-    const countValue = await AsyncStorage.getItem('@count');
+    /*const countValue = await AsyncStorage.getItem('@count');
     const count = parseInt(countValue);
-    setCounter(isNaN(count) ? 0 : count);
+    setCounter(isNaN(count) ? 0 : count);*/
+
+   
     //
     //const greetingInfo = await AsyncStorage.getItem('@greeting');
     // setCount(JSON.parse(greetingInfo));
@@ -151,20 +196,28 @@ export default function App() {
     setAnimalCounter(animalCounter + 1);
   }
   const funnyIncrementCounter = async () => {
-    await AsyncStorage.setItem('@count', (counter + 1).toString());
-    setCounter(counter + 1);
+    await AsyncStorage.setItem('@funnyCount', (funnyCounter + 1).toString());
+    setFunnyCounter(funnyCounter + 1);
   }
   const newsIncrementCounter = async () => {
-    await AsyncStorage.setItem('@count', (counter + 1).toString());
-    setCounter(counter + 1);
+    await AsyncStorage.setItem('@newsCount', (newsCounter + 1).toString());
+    setNewsCounter(newsCounter + 1);
   }
   const politicalIncrementCounter = async () => {
-    await AsyncStorage.setItem('@count', (counter + 1).toString());
-    setCounter(counter + 1);
+    await AsyncStorage.setItem('@politicalCount', (politicalCounter + 1).toString());
+    setPoliticalCounter(politicalCounter + 1);
   }
   const clearAsyncStorage = async() => {
     await AsyncStorage.setItem('@count', (0).toString());
     setCounter(0);
+    await AsyncStorage.setItem('@animalCount', (0).toString());
+    setAnimalCounter(0);
+    await AsyncStorage.setItem('@funnyCount', (0).toString());
+    setFunnyCounter(0);
+    await AsyncStorage.setItem('@newsCount', (0).toString());
+    setNewsCounter(0);
+    await AsyncStorage.setItem('@politicalCount', (0).toString());
+    setPoliticalCounter(0);
   } 
 
 
@@ -179,12 +232,25 @@ export default function App() {
   }*/
   function chooseIncrement(topic){
     var animalStr ='Animal';
-    alert('inside chooseIncrement');
+    var funnyStr ='Funny';
+    var newsStr ='News';
+    var politicalStr ='Political';
+
+    //alert('inside chooseIncrement');
+    //alert(topic);
     if (animalStr === topic){
-      alert('it is animal');
-      //animalIncrementCounter();
-    }else{
-      alert('not animal');
+      //alert('it is animal');
+      animalIncrementCounter();
+    }else if((funnyStr === topic)){
+      //alert('it is funny');
+      funnyIncrementCounter();
+    }else if((newsStr === topic)){
+      //alert('it is news');
+      newsIncrementCounter();
+    }
+    else{
+      //alert('it is political');
+      politicalIncrementCounter();
     }
 
   }
@@ -194,38 +260,43 @@ export default function App() {
       <Text style={styles.gridText}>{topic}</Text>
       <Image source={{uri: picture}}  
        style={{width: 50, height: 50}} />
-      <Button title="Increment Count" onPress={(topic)=>chooseIncrement(topic)} />  
+      <Button title="Increment Count" onPress={()=>chooseIncrement(topic)} />
+      {/*<Button title="Increment Count 2" onPress={()=>alert(topic)} />*/}
 
     </View>
   );
   return (
+
     <SafeAreaView style={styles.container}>
-      <FlatList
-      data={Topics}
-      renderItem={({item})=> <GridView topic={item.topic} picture={item.picture}/>}
-      keyExtractor={item => item.id}
-      numColumns={2}
-      key={item=> item.id}
-      />
-      <Text>Count: {counter}</Text>
-      <Text>Animal Count: {animalCounter}</Text>
-      <Text>funny Count: {funnyCounter}</Text>
-      <Text> news Count: {newsCounter}</Text>
-      <Text>political Count: {politicalCounter}</Text>
+      <View style={{width:"100%", padding: 12,justifyContent:'space-between'}}>
+        <FlatList
+        data={Topics}
+        renderItem={({item})=> <GridView topic={item.topic} picture={item.picture}/>}
+        keyExtractor={item => item.id}
+        numColumns={2}
+        key={item=> item.id}
+        />
+        <Text>Current state is: {appStateVisible}</Text>
+        <Text>Count: {counter}</Text>
+        <Text>Animal Count: {animalCounter}</Text>
+        <Text>funny Count: {funnyCounter}</Text>
+        <Text> news Count: {newsCounter}</Text>
+        <Text>political Count: {politicalCounter}</Text>
 
-      <Button title="Increment Count" onPress={incrementCounter} />
-      <Button title="Clear Count" onPress={clearAsyncStorage} />
-      <Button title="post test" onPress={()=>postTest()} />
-      <Button title="get test" onPress={()=>getTest()} />
+        <Button title="Increment Count" onPress={incrementCounter} />
+        <Button title="Clear Count" onPress={clearAsyncStorage} />
+        <Button title="post test" onPress={()=>postTest()} />
+        <Button title="get test" onPress={()=>getTest()} />
 
 
-      {/*<View style={styles.divider} />
-      <Text>Saved Greeting:</Text>
-      {greetingInfo ? <Text>{greetingInfo.greeting} {greetingInfo.name}</Text> : <Text>None :(</Text>}
-      <TextInput style={styles.input} onChangeText={setGreeting} placeholder="Greeting" value={greeting} />
-      <TextInput style={styles.input} onChangeText={setName} placeholder="Name" value={name} />
-  <Button title="Save Greeting" onPress={saveGreeting} />*/}
-      <StatusBar style="auto" />
+        {/*<View style={styles.divider} />
+        <Text>Saved Greeting:</Text>
+        {greetingInfo ? <Text>{greetingInfo.greeting} {greetingInfo.name}</Text> : <Text>None :(</Text>}
+        <TextInput style={styles.input} onChangeText={setGreeting} placeholder="Greeting" value={greeting} />
+        <TextInput style={styles.input} onChangeText={setName} placeholder="Name" value={name} />
+    <Button title="Save Greeting" onPress={saveGreeting} />*/}
+        <StatusBar style="auto" />
+      </View>
     </SafeAreaView>
   );
 }
